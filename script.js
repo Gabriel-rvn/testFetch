5c71dc3817a7cc0792c4f36f8b5ae65b094f7a56
Gabriel <gabriel.ravenin@gmail.com>
2026-01-27 21:57:11 -0300

    dar vida, mas nem tanta

--- script.js
+++ script.js
@@ -4,14 +4,40 @@
 //     .then(json => console.log(json))
 
 // --Versão que o Gemini ajudou a montar.
-fetch('https://jsonplaceholder.typicode.com/users/2')
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-    console.log("Dados recebidos:", data);
-    console.log("Nome do usuário:", data.name);
-  })
-  .catch(error => {
-    console.error("Algo deu errado:", error);
-  });
+// fetch('https://jsonplaceholder.typicode.com/users/2')
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then(data => {
+//     console.log("Dados recebidos:", data);
+//     console.log("Nome do usuário:", data.name);
+//   })
+//   .catch(error => {
+//     console.error("Algo deu errado:", error);
+//   });
+
+
+const tagName = document.getElementById("tagName");
+const searchButton = document.getElementById("searchTagName");
+
+async function getData() {
+    const inputValue = tagName.value;
+    const url = "https://jsonplaceholder.typicode.com/todos/";
+    const urlTagName = url.concat(inputValue);
+  
+    try {
+        const response = await fetch(urlTagName);
+        if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        console.log(result);
+    }catch (error) {
+        console.error(error.message);
+    };
+};
+
+searchButton.addEventListener("click", getData);
+
+
